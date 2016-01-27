af2f13aa1c600a7a21d4c5184a83c69dfb30af8c
chinaboard <chinaboard@gmail.com>
2016-01-27 21:54:46 +0800

    delete config file

--- PureCat/PureCat.cs
+++ PureCat/PureCat.cs
@@ -14,7 +14,6 @@ namespace PureCat
 {
     public class PureCat
     {
-        private static readonly string _configPath = Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, "App_Data\\TCConfig\\CatConfig.xml");
         private static readonly PureCat _instance = null;
         private static readonly object _lock = new object();
 
@@ -67,12 +66,6 @@ namespace PureCat
             Logger.Info("Cat .Net Client initialized.");
         }
 
-        public static void Initialize(string configFile = null)
-        {
-            ClientConfig clientConfig = LoadClientConfig(string.IsNullOrWhiteSpace(configFile) ? _configPath : configFile);
-
-            Initialize(clientConfig);
-        }
 
         public static bool IsInitialized()
         {
@@ -120,8 +113,6 @@ namespace PureCat
         {
             return GetProducer().CreateMessageId();
         }
-
-
 
         public static CatContext LogRemoteCallClient(string contextName)
         {
@@ -215,149 +206,10 @@ namespace PureCat
         {
             LogMetricInternal(name, "S,C", string.Format("{0},{1:F}", count, value));
         }
-
-
-
-
-
         private static void LogMetricInternal(string name, string status, string keyValuePairs = null)
         {
             GetProducer().LogMetric(name, status, keyValuePairs);
         }
-        #region 配置文件属性获取
-
-        private static ClientConfig LoadClientConfig(string configFile)
-        {
-            ClientConfig config = new ClientConfig();
-
-            if (File.Exists(configFile))
-            {
-                Logger.Info("Use config file({0}).", configFile);
-
-                XmlDocument doc = new XmlDocument();
-
-                doc.Load(configFile);
-
-                XmlElement root = doc.DocumentElement;
-
-                if (root != null)
-                {
-                    config.Domain = BuildDomain(root.GetElementsByTagName("domain"));
-
-                    IEnumerable<Server> servers = BuildServers(root.GetElementsByTagName("servers"));
-
-                    //NOTE: 只添加Enabled的
-                    foreach (Server server in servers.Where(server => server.Enabled))
-                    {
-                        config.Servers.Add(server);
-                        Logger.Info("CAT server configured: {0}:{1}", server.Ip, server.Port);
-                    }
-                }
-            }
-            else
-            {
-                Logger.Warn("Config file({0}) not found.", configFile);
-                //Logger.Warn("Config file({0}) not found, using localhost:2280 instead.", configFile);
-
-                //config.Domain = BuildDomain(null);
-                //config.Servers.Add(new Server("localhost", 2280));
-            }
-
-            return config;
-        }
-
-        private static Domain BuildDomain(XmlNodeList nodes)
-        {
-            if (nodes == null || nodes.Count == 0)
-            {
-                return new Domain();
-            }
-
-            XmlElement node = (XmlElement)nodes[0];
-            return new Domain
-            {
-                Id = GetStringProperty(node, "id", "Unknown"),
-                //Ip = GetStringProperty(node, "ip", null),
-                Enabled = GetBooleanProperty(node, "enabled", false)
-            };
-        }
-
-        private static IEnumerable<Server> BuildServers(XmlNodeList nodes)
-        {
-            List<Server> servers = new List<Server>();
-
-            if (nodes != null && nodes.Count > 0)
-            {
-                XmlElement first = (XmlElement)nodes[0];
-                XmlNodeList serverNodes = first.GetElementsByTagName("server");
-
-                foreach (XmlNode node in serverNodes)
-                {
-                    XmlElement serverNode = (XmlElement)node;
-                    string ip = GetStringProperty(serverNode, "ip", "localhost");
-                    int port = GetIntProperty(serverNode, "port", 2280);
-                    Server server = new Server(ip, port) { Enabled = GetBooleanProperty(serverNode, "enabled", true) };
-
-                    servers.Add(server);
-                }
-            }
-
-            if (servers.Count == 0)
-            {
-                Logger.Warn("No server configured, use localhost:2280 instead.");
-                servers.Add(new Server("localhost", 2280));
-            }
-
-            return servers;
-        }
-
-        private static string GetStringProperty(XmlElement element, string name, string defaultValue)
-        {
-            if (element != null)
-            {
-                string value = element.GetAttribute(name);
-
-                if (value.Length > 0)
-                {
-                    return value;
-                }
-            }
-
-            return defaultValue;
-        }
-
-        private static bool GetBooleanProperty(XmlElement element, string name, bool defaultValue)
-        {
-            if (element != null)
-            {
-                string value = element.GetAttribute(name);
-
-                if (value.Length > 0)
-                {
-                    return "true".Equals(value);
-                }
-            }
-
-            return defaultValue;
-        }
-
-        private static int GetIntProperty(XmlElement element, string name, int defaultValue)
-        {
-            if (element != null)
-            {
-                string value = element.GetAttribute(name);
-
-                if (value.Length > 0)
-                {
-                    int tmpRet;
-                    if (int.TryParse(value, out tmpRet))
-                        return tmpRet;
-                }
-            }
-
-            return defaultValue;
-        }
-        #endregion
 
     }
 }
