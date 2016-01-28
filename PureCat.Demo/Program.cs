f7ff6e4e1b07cac485d6fca25d0d1a41ec78dc0d
chinaboard <chinaboard@gmail.com>
2016-01-28 20:15:24 +0800

    bump version

---
+++ PureCat.Demo/Program.cs
@@ -0,0 +1,47 @@
+﻿using PureCat.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PureCat.Demo
+{
+    class Program
+    {
+        static Random _rand = new Random();
+        static void Main(string[] args)
+        {
+            PureCat.Initialize(new Configuration.ClientConfig(new Configuration.Domain("PureCat.Demo"), new Configuration.Server("10.14.40.4")));
+            while (true)
+            {
+                var a = DateTime.Now.Second;
+                Console.WriteLine(DateTime.Now);
+                var context = PureCat.DoTransaction("Do", nameof(DoTest), DoTest);
+
+                var b = DateTime.Now.Second;
+                Task.Factory.StartNew(() =>
+                {
+                    PureCat.DoTransaction("Do", nameof(Add), () => Add(a, b, context));
+                });
+            }
+
+        }
+
+
+        static CatContext DoTest()
+        {
+            var times = _rand.Next(1000);
+            Thread.Sleep(times);
+            PureCat.LogEvent("Do", nameof(DoTest), "0", $"sleep {times}");
+            return PureCat.LogRemoteCallClient("callAdd");
+        }
+
+        static void Add(int a, int b, CatContext context = null)
+        {
+            PureCat.LogRemoteCallServer(context);
+            PureCat.LogEvent("Do", nameof(Add), "0", $"{a} + {b} = {a + b}");
+        }
+    }
+}
--- PureCat.Demo/Properties/AssemblyInfo.cs
+++ PureCat.Demo/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("PureCat")]
-[assembly: AssemblyDescription("Cat.Net Client")]
+[assembly: AssemblyTitle("PureCat.Demo")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("PureCat")]
-[assembly: AssemblyCopyright("Copyright © chinaboard 2016")]
+[assembly: AssemblyProduct("PureCat.Demo")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
-[assembly: Guid("df6be1db-4713-4456-9a2a-6e3c8af94a24")]
+[assembly: Guid("7f92d3ac-6d63-46b5-a9f3-ef2dbef1f348")]
 
 // 程序集的版本信息由下列四个值组成: 
 //
@@ -32,4 +32,5 @@ using System.Runtime.InteropServices;
 //可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值，
 // 方法是按如下所示使用“*”: :
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.1.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
