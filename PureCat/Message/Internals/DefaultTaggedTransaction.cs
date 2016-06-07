f64fdf2e4a663ef5a3694d4702bdf0614d063212
chinaboard <chinaboard@gmail.com>
2016-06-07 10:49:28 +0800

    modify DefaultTaggedTransaction code

--- PureCat/Message/Internals/DefaultTaggedTransaction.cs
+++ PureCat/Message/Internals/DefaultTaggedTransaction.cs
@@ -5,8 +5,8 @@ using System.Collections.Generic;
 
 namespace PureCat.Message.Internals
 {
-    
-    public class DefaultTaggedTransaction : AbstractMessage, ITaggedTransaction
+
+    public class DefaultTaggedTransaction : DefaultTransaction, ITaggedTransaction
     {
         private IList<IMessage> _mChildren;
         private long _mDurationInMicro; // must be less than 0
@@ -21,54 +21,6 @@ namespace PureCat.Message.Internals
 
         public string Tag { get { return _tag; } }
 
-        public IList<IMessage> Children
-        {
-            get { return _mChildren ?? (_mChildren = new List<IMessage>()); }
-        }
-
-        public long DurationInMicros
-        {
-            get
-            {
-                if (_mDurationInMicro >= 0)
-                {
-                    return _mDurationInMicro;
-                }
-                // if it's not completed explicitly
-                long duration = 0;
-                int len = (_mChildren == null) ? 0 : _mChildren.Count;
-
-                if (len > 0)
-                {
-                    if (_mChildren != null)
-                    {
-                        IMessage lastChild = _mChildren[len - 1];
-
-                        if (lastChild is ITransaction)
-                        {
-                            ITransaction trx = lastChild as ITransaction;
-
-                            duration = trx.Timestamp * 1000L + trx.DurationInMicros - TimestampInMicros;
-                        }
-                        else
-                        {
-                            duration = lastChild.Timestamp * 1000L - TimestampInMicros;
-                        }
-                    }
-                }
-
-                return duration;
-            }
-            set { _mDurationInMicro = value; }
-        }
-
-        public long DurationInMillis
-        {
-            get { return DurationInMicros / 1000L; }
-            set { _mDurationInMicro = value * 1000L; }
-        }
-
-        public bool Standalone { get; set; }
 
         public DefaultTaggedTransaction(string type, string name, string tag, IMessageManager messageManager)
            : base(type, name, messageManager)
@@ -83,30 +35,6 @@ namespace PureCat.Message.Internals
                 _rootMessageId = tree.RootMessageId;
                 _parentMessageId = tree.ParentMessageId;
             }
-        }
-
-        public ITransaction AddChild(IMessage message)
-        {
-            if (_mChildren == null)
-            {
-                _mChildren = new List<IMessage>();
-            }
-
-            if (message != null)
-            {
-                _mChildren.Add(message);
-            }
-            else
-            {
-                PureCatClient.GetProducer().LogError(new Exception("null child message"));
-            }
-
-            return this;
-        }
-
-        public bool HasChildren()
-        {
-            return _mChildren != null && _mChildren.Count > 0;
         }
 
         public void Bind(string tag, string childMessageId, string title = null)
@@ -130,7 +58,5 @@ namespace PureCat.Message.Internals
                 tree.RootMessageId = _rootMessageId;
             }
         }
-
-
     }
 }
